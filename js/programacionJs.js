b464e5f8d28967cab3009e211fa56836b4449ab9
Tomas Mauricio Rodriguez Sogamoso <thomasmauricio521@gmail.com>
2022-01-10 18:46:00 -0500

    nuevo

--- js/programacionJs.js
+++ js/programacionJs.js
@@ -1,2 +1,5 @@
-document.getElementById("mensaje").innerHTML = "My First JavaScript";
 document.getElementById("nomPagina").innerHTML = "JavaScript";
+
+function cambioParrafo() {
+  document.getElementById("mensaje").innerHTML = "My First JavaScript";
+}
